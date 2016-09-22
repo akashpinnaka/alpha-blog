a99463a6f71c9f98e8503cfd929bde8d0c498c1d
akashpinnaka <aakash.pinnaka@gmail.com>
2016-09-23 00:01:04 +0530

    Add login, logout functionality for users

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'bcrypt', '~> 3.1', '>= 3.1.11'
+gem 'will_paginate', '~> 3.1', '>= 3.1.3'
+gem 'bootstrap-will_paginate', '~> 0.0.10'
 
 group :development, :test do 
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :articles
   resources :users, except: [:new]
 end
